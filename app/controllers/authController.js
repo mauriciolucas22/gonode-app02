8482f68521838de6b3dfa43178e16bf7b190722e
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-08-11 09:40:16 -0300

    links and scripts added in singin

---
+++ app/controllers/authController.js
@@ -0,0 +1,15 @@
+const { User } = require('../models');
+
+module.exports = {
+  /* async index(req, res) {
+    const users = await User.findAll();
+    res.render('index', { users });
+  }, */
+  singin(req, res) {
+    return res.render('auth/singin');
+  },
+
+  singup(req, res) {
+    return res.render('auth/singup');
+  },
+};
