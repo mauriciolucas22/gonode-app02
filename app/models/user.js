1dabf3d7810755ff6b8cc9d2c013eae5bffe41dd
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-08-10 22:21:31 -0300

    create user ok

--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,22 @@
+/**
+ *
+ * @param {*} sequelize
+ * @param {*} DataTypes
+ *
+ * DataTypes: usado nas migrations
+ */
 module.exports = (sequelize, DataTypes) => {
+  /**
+   * Passa o nome do model, sempre o nome da tabela no singular
+   * define as colunas dessa tabela
+   * campos id, createAt, updateAt => são preenchidos automaticamente
+   */
 
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  });
+
+  return User;
 };
--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const nunjucks = require('nunjucks');
 
 const app = express();
 
+// imports de models: retornas todos os models criados
+const { User } = require('./app/models');
+
+User.create({ name: 'Jesus', email: '@jesus', password: '123' });
+
 nunjucks.configure(path.resolve('app', 'views'), {
   autoescape: true,
   express: app,
