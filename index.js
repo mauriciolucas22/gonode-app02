7a058bbac10d1834b28470243de74a5eeaa17a25
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-08-10 22:30:00 -0300

    update documentation

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const nunjucks = require('nunjucks');
 const app = express();
 
 // imports de models: retornas todos os models criados
-const { User } = require('./app/models');
+// const { User } = require('./app/models');
 
-User.create({ name: 'Jesus', email: '@jesus', password: '123' });
+// User.create({ name: 'Jesus', email: '@jesus', password: '123' });
 
 nunjucks.configure(path.resolve('app', 'views'), {
   autoescape: true,
@@ -17,9 +17,5 @@ nunjucks.configure(path.resolve('app', 'views'), {
 
 app.set('view engine', 'njk');
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
 
 app.listen(3000);
