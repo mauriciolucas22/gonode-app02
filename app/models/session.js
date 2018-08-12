c4299e3f1561e7a0491c45028dc59d01bb72b9d4
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-08-12 09:21:18 -0300

    sessions in database ok

---
+++ app/models/session.js
@@ -0,0 +1,15 @@
+/**
+ * utilizado para armazenar as sessions
+ */
+module.exports = (sequelize, DataTypes) => {
+  const Session = sequelize.define('Session', {
+    sid: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    expires: DataTypes.DATE,
+    data: DataTypes.TEXT,
+  });
+
+  return Session;
+};
